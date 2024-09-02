a0cfe5922cebacc46d55d3c3fc9ef9ccc2daeaca
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-09-02 22:28:59 +0900

    Create useAppPageStack hook

--- src/pages/v2.tsx
+++ src/pages/v2.tsx
@@ -1,10 +1,32 @@
+import { useRef, useEffect } from "react";
+import qs from "query-string";
+
 import { GlobalStateProvider } from "v2/globalState";
 import { TaskListList } from "v2/components/TaskListList";
 import { TaskList } from "v2/components/TaskList";
-
 import { useApp } from "v2/hooks";
 
+const appPath = "/v2";
+
+function useAppPageStack() {
+  const isInitialRender = useRef(true);
+  useEffect(() => {
+    const isFastRefresh = !isInitialRender.current;
+    if (!isFastRefresh) {
+      const query = qs.parse(window.location.search);
+      if (query.sheet) {
+        const tmp = window.location.href;
+        window.history.replaceState({}, "", appPath);
+        window.history.pushState({}, "", tmp);
+      }
+    }
+    isInitialRender.current = false;
+  }, []);
+}
+
 function AppV2Content() {
+  useAppPageStack();
+
   const [{ data: app }] = useApp();
 
   return (

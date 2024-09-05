9a25d3a880c6accd716eba33fede3f5e354f74e1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-09-05 23:34:17 +0900

    Use AuthProvider

--- src/pages/v2.tsx
+++ src/pages/v2.tsx
@@ -1,57 +1,27 @@
-import { useRef, useEffect } from "react";
-import qs from "query-string";
+import { useRouter } from "next/router";
 
+import { useAuth, AuthProvider } from "v2/auth";
 import { GlobalStateProvider } from "v2/globalState";
-import { TaskListList } from "v2/components/TaskListList";
-import { TaskList } from "v2/components/TaskList";
-import { useApp } from "v2/hooks";
+import { App } from "v2/components/App";
 
-function useAppPageStack() {
-  const isInitialRender = useRef(true);
-  useEffect(() => {
-    const isFastRefresh = !isInitialRender.current;
-    if (!isFastRefresh) {
-      const query = qs.parse(window.location.search);
-      if (query.sheet) {
-        const tmp = window.location.href;
-        window.history.replaceState({}, "", window.location.pathname);
-        window.history.pushState({}, "", tmp);
-      }
-    }
-    isInitialRender.current = false;
-  }, []);
-}
+function Content() {
+  const router = useRouter();
+  const [{ isInitialized, isLoggedIn }] = useAuth();
 
-function AppV2Content() {
-  useAppPageStack();
+  if (isInitialized && !isLoggedIn) {
+    router.push("/login");
+    return null;
+  }
 
-  const [{ data: app }] = useApp();
-
-  return (
-    <div className="flex">
-      <div className="border-r p-4">
-        <TaskListList />
-      </div>
-
-      <div className="flex-1">
-        <div className="flex">
-          {app.taskListIds.map((taskListId) => {
-            return (
-              <div key={taskListId} className="flex-1">
-                <TaskList taskListId={taskListId} />
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+  return isInitialized ? <App /> : <div>Loading...</div>;
 }
 
 export default function AppV2Page() {
   return (
-    <GlobalStateProvider>
-      <AppV2Content />
-    </GlobalStateProvider>
+    <AuthProvider>
+      <GlobalStateProvider>
+        <Content />
+      </GlobalStateProvider>
+    </AuthProvider>
   );
 }

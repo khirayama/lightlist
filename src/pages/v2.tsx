29d8757a10bc89408fee708fece2cd22bf62a0dc
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-09-07 20:19:38 +0900

    Update v2 page

--- src/pages/v2.tsx
+++ src/pages/v2.tsx
@@ -3,17 +3,30 @@ import { useRouter } from "next/router";
 import { useAuth, AuthProvider } from "v2/auth";
 import { GlobalStateProvider } from "v2/globalState";
 import { App } from "v2/components/App";
+import { useApp } from "v2/hooks/useApp";
+import { usePreferences } from "v2/hooks/usePreferences";
+import { useTaskLists } from "v2/hooks/useTaskLists";
 
 function Content() {
   const router = useRouter();
   const [{ isInitialized, isLoggedIn }] = useAuth();
+  const [{ isInitialized: isAppInitialized }] = useApp();
+  const [{ isInitialized: isPreferencesInitialized }] = usePreferences();
+  const [{ isInitialized: isTaskListsInitialized }] = useTaskLists();
 
   if (isInitialized && !isLoggedIn) {
     router.push("/login");
     return null;
   }
 
-  return isInitialized ? <App /> : <div>Loading...</div>;
+  return isInitialized &&
+    isAppInitialized &&
+    isPreferencesInitialized &&
+    isTaskListsInitialized ? (
+    <App />
+  ) : (
+    <div>Loading...</div>
+  );
 }
 
 export default function AppV2Page() {

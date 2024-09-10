fa4dfd21634ec1a2599a9c190a850c8562a7a48e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-09-11 08:49:50 +0900

    Create DrawerLayout component

---
+++ src/v2/components/primitives/DrawerLayout.tsx
@@ -0,0 +1,99 @@
+import { useState, useEffect, ReactNode } from "react";
+import { clsx } from "clsx";
+import { useRouter } from "next/router";
+import qs from "query-string";
+
+export function isNarrowLayout() {
+  return window.innerWidth < 768;
+}
+
+export function useDrawerLayout(): {
+  open: boolean;
+  isNarrowLayout: boolean;
+  close: () => void;
+} {
+  const router = useRouter();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(isDrawerOpened());
+  const [isNarrow, setIsNarrow] = useState(isNarrowLayout());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsNarrow(isNarrowLayout());
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isNarrowLayout(), isDrawerOpen]);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      const isOpened = isDrawerOpened();
+      setIsDrawerOpen(isOpened);
+      setTimeout(() => {
+        const p = isOpened
+          ? document.querySelector<HTMLElement>("[data-sectiondrawer]")
+          : document.querySelector<HTMLElement>("[data-sectionmain]");
+        p?.focus();
+      }, 1);
+    };
+
+    handleRouteChange();
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
+  return {
+    open: isDrawerOpen,
+    isNarrowLayout: isNarrow,
+    close: () => router.back(),
+  };
+}
+
+function isDrawerOpened() {
+  return qs.parse(window.location.search).drawer === "opened";
+}
+
+export const Drawer = (props: {
+  open: boolean;
+  isNarrowLayout: boolean;
+  close: () => void;
+  children?: ReactNode;
+}) => {
+  return (
+    <section
+      data-sectiondrawer
+      className={clsx(
+        "bg absolute z-30 h-full w-full min-w-[320px] -translate-x-full transition-transform duration-[320ms] md:relative md:block md:w-[auto] md:max-w-sm md:translate-x-0",
+        props.open && "translate-x-0",
+        !props.isNarrowLayout && "border-r",
+      )}
+    >
+      {props.children}
+    </section>
+  );
+};
+
+export const Main = (props: {
+  open: boolean;
+  close: () => void;
+  children?: ReactNode;
+}) => {
+  return (
+    <section data-sectionmain className="h-full w-full min-w-[375px]">
+      {props.children}
+    </section>
+  );
+};
+
+export const DrawerLayout = (props: { children: ReactNode }) => {
+  return (
+    <div className="bg flex h-full w-full overflow-hidden">
+      {props.children}
+    </div>
+  );
+};

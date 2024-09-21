83939de3da934c1e471f3f71819e2fc095511df1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-09-21 15:56:56 +0900

    Update TaskListList v2

--- src/v2/components/app/TaskListList.tsx
+++ src/v2/components/app/TaskListList.tsx
@@ -2,32 +2,55 @@ import { useState } from "react";
 
 import { useApp } from "v2/hooks/app/useApp";
 import { useTaskLists } from "v2/hooks/app/useTaskLists";
+import { useCustomTranslation } from "v2/common/i18n";
+import { Icon } from "v2/components/primitives/Icon";
 
-export function TaskListList() {
+export function TaskListList(props: { disabled?: boolean }) {
+  const { t } = useCustomTranslation("components.TaskListList");
+
   const [taskListName, setTaskListName] = useState("");
   const [{ data: app }] = useApp();
   const [{ data: taskLists }, { appendTaskList, deleteTaskList }] =
     useTaskLists(app?.taskListIds || []);
 
+  const onTaskListFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (taskListName === "") {
+      return;
+    }
+    appendTaskList({ name: taskListName });
+    setTaskListName("");
+    /* TODO: Close drawer and scroll to new task */
+  };
+
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (taskListName) {
-            appendTaskList({ name: taskListName });
-            setTaskListName("");
-          }
-        }}
-      >
-        <input
-          className="border"
-          type="text"
-          value={taskListName}
-          onChange={(e) => setTaskListName(e.currentTarget.value)}
-        />
-        <button>リストを作成</button>
-      </form>
+      <header className="sticky top-0 z-20 w-full">
+        <section className="px-1">
+          <form
+            className="flex items-center py-2"
+            onSubmit={onTaskListFormSubmit}
+          >
+            <div className="flex-1 pl-2">
+              <input
+                disabled={props.disabled}
+                className="w-full rounded-full border px-4 py-2 focus-visible:bg-gray-200 dark:focus-visible:bg-gray-800"
+                type="text"
+                value={taskListName}
+                placeholder={t("Add task list to bottom")}
+                onChange={(e) => setTaskListName(e.currentTarget.value)}
+              />
+            </div>
+            <button
+              disabled={props.disabled}
+              className="flex rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
+              type="submit"
+            >
+              <Icon text="send" />
+            </button>
+          </form>
+        </section>
+      </header>
 
       {taskLists.map((taskList) => {
         return (

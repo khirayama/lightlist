feb76ab3a41f02b98012582f588d42c20dcbaee0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-10-03 21:18:44 +0900

    Update useAppPageStack

--- src/v2/hooks/ui/useAppPageStack.tsx
+++ src/v2/hooks/ui/useAppPageStack.tsx
@@ -1,16 +1,19 @@
 import { useRef, useEffect } from "react";
 import qs from "query-string";
+import { useRouter } from "next/router";
 
 export function useAppPageStack() {
+  const router = useRouter();
+
   const isInitialRender = useRef(true);
   useEffect(() => {
     const isFastRefresh = !isInitialRender.current;
     if (!isFastRefresh) {
       const query = qs.parse(window.location.search);
-      if (query.sheet) {
+      if (query.sheet || query.drawer === "opened") {
         const tmp = window.location.href;
-        window.history.replaceState({}, "", window.location.pathname);
-        window.history.pushState({}, "", tmp);
+        router.replace(window.location.origin + window.location.pathname);
+        router.push(tmp);
       }
     }
     isInitialRender.current = false;

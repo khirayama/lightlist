579c6b811450ea315c18551eb65fba983940444e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-10-10 21:55:36 +0900

    Update useAppNavigation

--- src/v2/hooks/ui/useAppNavigation.tsx
+++ src/v2/hooks/ui/useAppNavigation.tsx
@@ -1,8 +1,20 @@
-import { useRef, useEffect } from "react";
+import {
+  createContext,
+  useEffect,
+  ReactNode,
+  useRef,
+  useContext,
+  KeyboardEvent,
+} from "react";
 import qs from "query-string";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
-export function useAppPageStack() {
+type Query = { [key: string]: string | string[] | null };
+
+const AppPageStackContext = createContext(null);
+
+export const AppPageStackProvider = (props: { children: ReactNode }) => {
   const router = useRouter();
 
   const isInitialRender = useRef(true);
@@ -18,4 +30,61 @@ export function useAppPageStack() {
     }
     isInitialRender.current = false;
   }, []);
+
+  return (
+    <AppPageStackContext.Provider
+      value={{
+        pop: () => {
+          router.back();
+        },
+        push: (url: string) => {
+          router.push(url);
+        },
+        replace: (url: string) => {
+          router.replace(url);
+        },
+        pushWithParams: (
+          pathname: string,
+          options?: {
+            params?: Query;
+            mergeParams?: boolean;
+          },
+        ) => {
+          const q = options.mergeParams
+            ? { ...qs.parse(window.location.search), ...(options.params || {}) }
+            : options.params || {};
+          const s = qs.stringify(q);
+          const url = pathname + (s ? `?${s}` : "");
+          router.push(url);
+        },
+      }}
+    >
+      {props.children}
+    </AppPageStackContext.Provider>
+  );
+};
+
+export const useAppPageStack = () => {
+  return useContext(AppPageStackContext);
+};
+
+export function AppPageLink(props: {
+  href?: string;
+  params?: Query;
+  mergeParams?: boolean;
+  replace?: boolean;
+  className?: string;
+  tabIndex?: number;
+  children: ReactNode;
+  onKeyDown?: (e?: KeyboardEvent<HTMLAnchorElement>) => void;
+}) {
+  const p = { ...props };
+  const q = props.mergeParams
+    ? { ...qs.parse(window.location.search), ...props.params }
+    : props.params;
+  const s = qs.stringify(q);
+  const href = (props.href || window.location.pathname) + (s ? `?${s}` : "");
+  delete p.mergeParams;
+  delete p.params;
+  return <Link {...{ ...p, href }}>{props.children}</Link>;
 }

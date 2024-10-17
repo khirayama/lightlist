d7cbf18440772f5ce550e0f38e17cb0d90becaaa
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-10-17 20:51:12 +0900

    Update App component

--- src/v2/components/app/App.tsx
+++ src/v2/components/app/App.tsx
@@ -10,17 +10,82 @@ import { TaskList } from "v2/components/app/TaskList";
 import { TaskListList } from "v2/components/app/TaskListList";
 import { useApp } from "v2/hooks/app/useApp";
 import { usePreferences } from "v2/hooks/app/usePreferences";
-import { useAppPageStack } from "v2/hooks/ui/useAppNavigation";
 import { useTheme } from "v2/hooks/ui/useTheme";
 import { useActiveStatus } from "v2/hooks/composites/useActiveStatus";
 import { Icon } from "v2/components/primitives/Icon";
-import { ParamsLink } from "v2/components/primitives/ParamsLink";
+import { useAppPageStack, AppPageLink } from "v2/hooks/ui/useAppNavigation";
+
+function AppDrawer() {
+  const { pop } = useAppPageStack();
+  const { isNarrowLayout } = useDrawerLayout();
+
+  return (
+    <Drawer>
+      {isNarrowLayout && (
+        <div className="flex">
+          <div className="p-1">
+            <button
+              className="rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
+              onClick={pop}
+            >
+              <Icon text="close" />
+            </button>
+          </div>
+          <div className="flex-1" />
+        </div>
+      )}
+
+      <div className="p-2">
+        <AppPageLink
+          data-trigger="user"
+          className="flex w-full items-center justify-center rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
+          params={{ sheet: "user", trigger: "user" }}
+          mergeParams
+        >
+          <Icon text="person" />
+          <div className="flex-1 pl-2 text-left">Log in</div>
+        </AppPageLink>
+
+        <AppPageLink
+          data-trigger="settings"
+          className="flex w-full items-center justify-center rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
+          params={{ sheet: "settings", trigger: "settings" }}
+          mergeParams
+        >
+          <Icon text="settings" />
+          <div className="flex-1 pl-2 text-left">Preferences</div>
+        </AppPageLink>
+      </div>
+
+      <TaskListList />
+    </Drawer>
+  );
+}
+
+function AppMain() {
+  const [{ data: app }] = useApp();
+  const { isNarrowLayout } = useDrawerLayout();
+
+  return (
+    <Main>
+      {isNarrowLayout && (
+        <AppPageLink params={{ drawer: "opened" }}>Open</AppPageLink>
+      )}
+      <div className="flex">
+        {app.taskListIds.map((taskListId) => {
+          return (
+            <div key={taskListId} className="flex-1">
+              <TaskList taskListId={taskListId} />
+            </div>
+          );
+        })}
+      </div>
+    </Main>
+  );
+}
 
 export function App() {
-  useAppPageStack();
   useActiveStatus();
-
-  const da = useDrawerLayout();
 
   const [{ data: app }] = useApp();
   const [{ data: preferences }] = usePreferences();
@@ -38,61 +103,8 @@ export function App() {
       </Head>
 
       <DrawerLayout>
-        <Drawer {...da}>
-          {da.isNarrowLayout && (
-            <div>
-              <button onClick={da.close}>Close</button>
-            </div>
-          )}
-
-          <div className="flex md:hidden">
-            <div className="p-1">
-              <button className="rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700">
-                <Icon text="close" />
-              </button>
-            </div>
-            <div className="flex-1" />
-          </div>
-
-          <div className="p-2">
-            <ParamsLink
-              data-trigger="user"
-              className="flex w-full items-center justify-center rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
-              params={{ sheet: "user", trigger: "user" }}
-              mergeParams
-            >
-              <Icon text="person" />
-              <div className="flex-1 pl-2 text-left">Log in</div>
-            </ParamsLink>
-
-            <ParamsLink
-              data-trigger="settings"
-              className="flex w-full items-center justify-center rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
-              params={{ sheet: "settings", trigger: "settings" }}
-              mergeParams
-            >
-              <Icon text="settings" />
-              <div className="flex-1 pl-2 text-left">Preferences</div>
-            </ParamsLink>
-          </div>
-
-          <TaskListList />
-        </Drawer>
-
-        <Main {...da}>
-          {da.isNarrowLayout && (
-            <ParamsLink params={{ drawer: "opened" }}>Open</ParamsLink>
-          )}
-          <div className="flex">
-            {app.taskListIds.map((taskListId) => {
-              return (
-                <div key={taskListId} className="flex-1">
-                  <TaskList taskListId={taskListId} />
-                </div>
-              );
-            })}
-          </div>
-        </Main>
+        <AppDrawer />
+        <AppMain />
       </DrawerLayout>
     </>
   );

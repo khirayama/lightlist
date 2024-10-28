6e14b540468f620410e55f900102ba4ed49d3793
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-10-28 22:42:48 +0900

    Update Carousel

--- src/v2/components/primitives/Carousel.tsx
+++ src/v2/components/primitives/Carousel.tsx
@@ -23,40 +23,39 @@ const scrollDebounce = createDebounce();
 const CarouselContext = createContext(null);
 
 export function CarouselIndicator() {
-  const [{ index, length }, { setIndex }] = useContext(CarouselContext);
+  const [{ index, length }, { onIndexChange }] = useContext(CarouselContext);
   const indicator = Array.from({ length }, (_, i) => i);
 
   return (
     <div>
-      <ul>
-        {indicator.map((i) => {
-          return (
-            <li key={i} onClick={() => setIndex(i)}>
-              {i === index ? "●" : "○"}
-            </li>
-          );
-        })}
-      </ul>
+      {indicator.map((i) => {
+        return (
+          <button
+            key={i}
+            onClick={() => {
+              onIndexChange(i);
+            }}
+          >
+            {i === index ? "●" : "○"}
+          </button>
+        );
+      })}
     </div>
   );
 }
 
-export function CarouselSection(props: {
+export function Carousel(props: {
   children: ReactNode;
+  index: number;
   onIndexChange: (index: number) => void;
 }) {
-  const [index, setIndex] = useState(0);
   const [length, setLength] = useState(Children.count(props.children));
-
-  useEffect(() => {
-    props.onIndexChange(index);
-  }, [index]);
 
   return (
     <CarouselContext.Provider
       value={[
-        { index, length },
-        { setIndex, setLength },
+        { index: props.index, length },
+        { setLength, onIndexChange: props.onIndexChange },
       ]}
     >
       {props.children}
@@ -64,8 +63,8 @@ export function CarouselSection(props: {
   );
 }
 
-export function Carousel(props: { children: ReactNode }) {
-  const [{ index }, { setIndex, setLength }] = useContext(CarouselContext);
+export function CarouselList(props: { children: ReactNode }) {
+  const [{ index }, { setLength, onIndexChange }] = useContext(CarouselContext);
   const ref = useRef(null);
 
   useEffect(() => {
@@ -74,7 +73,15 @@ export function Carousel(props: { children: ReactNode }) {
 
   useEffect(() => {
     const el = ref.current.childNodes[index];
-    el.scrollIntoView({ behavior: "smooth" });
+    el.parentNode.scrollLeft = el.offsetLeft;
+  }, []);
+
+  useEffect(() => {
+    const el = ref.current.childNodes[index];
+    el.parentNode.scrollTo({
+      left: el.offsetLeft,
+      behavior: "smooth",
+    });
   }, [index]);
 
   useEffect(() => {
@@ -83,19 +90,18 @@ export function Carousel(props: { children: ReactNode }) {
         const els = ref.current.childNodes;
         for (let i = 0; i < els.length; i++) {
           if (Math.abs(els[i].offsetLeft - ref.current.scrollLeft) < 10) {
-            setIndex(i);
+            onIndexChange(i);
             break;
           }
         }
       }, 30);
     };
 
-    handleScroll();
     ref.current?.addEventListener("scroll", handleScroll);
     return () => {
       ref.current?.removeEventListener("scroll", handleScroll);
     };
-  }, [setIndex]);
+  }, [index]);
 
   return (
     <section

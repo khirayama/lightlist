51976befbb58a1ca0d9d7182cd681f82fb7a9ca2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-11-06 22:49:07 +0900

    Remove defined TaskListItem

--- src/v2/components/app/TaskList.tsx
+++ src/v2/components/app/TaskList.tsx
@@ -1,53 +1,7 @@
 import { useState } from "react";
 
 import { useTaskLists } from "v2/hooks/app/useTaskLists";
-
-function TaskListItem(props: { taskListId: string; task: TaskV2 }) {
-  const [, { updateTask, deleteTask }] = useTaskLists();
-
-  return (
-    <div className="">
-      <input
-        className="h-[20px] w-[20px] bg-gray-400 checked:bg-blue-400"
-        type="checkbox"
-        checked={props.task.completed}
-        onChange={(e) => {
-          updateTask(props.taskListId, {
-            ...props.task,
-            completed: e.currentTarget.checked,
-          });
-        }}
-      />
-      <input
-        type="text"
-        value={props.task.text}
-        onChange={(e) => {
-          updateTask(props.taskListId, {
-            ...props.task,
-            text: e.currentTarget.value,
-          });
-        }}
-      />
-      <input
-        type="date"
-        value={props.task.date || ""}
-        onChange={(e) => {
-          updateTask(props.taskListId, {
-            ...props.task,
-            date: e.currentTarget.value,
-          });
-        }}
-      />
-      <button
-        onClick={() => {
-          deleteTask(props.taskListId, props.task.id);
-        }}
-      >
-        [x]
-      </button>
-    </div>
-  );
-}
+import { TaskListItem } from "v2/components/app/TaskListItem";
 
 export function TaskList(props: { taskListId: string }) {
   const [taskText, setTaskText] = useState("");

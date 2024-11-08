f04111941f91404bd90bd5e2779cb46587c72c8f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-11-08 22:26:04 +0900

    Update TaskList v2

--- src/v2/components/app/TaskList.tsx
+++ src/v2/components/app/TaskList.tsx
@@ -1,41 +1,163 @@
-import { useState } from "react";
+import { useState, FormEvent, KeyboardEvent } from "react";
 
 import { useTaskLists } from "v2/hooks/app/useTaskLists";
+import { useApp } from "v2/hooks/app/useApp";
 import { TaskListItem } from "v2/components/app/TaskListItem";
+import { Icon } from "v2/components/primitives/Icon";
+import { AppPageLink } from "v2/hooks/ui/useAppNavigation";
+import { useCustomTranslation } from "v2/common/i18n";
 
-export function TaskList(props: { taskListId: string }) {
+export function TaskList(props: { disabled?: boolean; taskListId: string }) {
+  const { t } = useCustomTranslation("components.TaskList");
+
   const [taskText, setTaskText] = useState("");
-  const [{ data: taskLists }, { appendTask, updateTaskList, sortTasks }] =
-    useTaskLists([props.taskListId]);
+  const [isShiftPressed, setIsShiftPressed] = useState<boolean>(false);
+  const [
+    { data: taskLists },
+    { appendTask, prependTask, updateTaskList, sortTasks, clearCompletedTasks },
+  ] = useTaskLists([props.taskListId]);
   const taskList = taskLists.find((tl) => tl.id === props.taskListId);
+  const [{ data: app }, { updateApp }] = useApp();
+
+  const isInsertTop =
+    (app.taskInsertPosition === "TOP" && !isShiftPressed) ||
+    (app.taskInsertPosition === "BOTTOM" && isShiftPressed);
+
+  const onTaskListNameChange = (e: FormEvent<HTMLInputElement>) => {
+    updateTaskList({ ...taskList, name: e.currentTarget.value });
+  };
+
+  const onInsertPositionIconClick = () => {
+    updateApp({
+      taskInsertPosition:
+        app.taskInsertPosition === "BOTTOM" ? "TOP" : "BOTTOM",
+    });
+  };
+
+  const onTaskTextChange = (e: FormEvent<HTMLInputElement>) => {
+    setTaskText(e.currentTarget.value);
+  };
+
+  const onTaskTextKeyDownAndKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    setIsShiftPressed(e.shiftKey);
+  };
+
+  const onTaskTextBlur = () => {
+    setIsShiftPressed(false);
+  };
+
+  const onTaskFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (taskText === "") {
+      return;
+    }
+    if (isInsertTop) {
+      prependTask(props.taskListId, { text: taskText });
+    } else {
+      appendTask(props.taskListId, { text: taskText });
+    }
+    setTaskText("");
+  };
+
+  const onSortTasksButtonClick = () => {
+    sortTasks(taskList.id);
+  };
+
+  const onClearCompletedTasksButtonClick = () => {
+    clearCompletedTasks(taskList.id);
+  };
 
   return (
     <div className="bg h-full overflow-scroll">
       <header className="bg sticky top-0 z-20 w-full border-b">
-        <input
-          type="text"
-          value={taskList.name}
-          onChange={(e) => {
-            updateTaskList({ ...taskList, name: e.currentTarget.value });
-          }}
-        />
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (taskText) {
-              appendTask(props.taskListId, { text: taskText });
-              setTaskText("");
-            }
-          }}
-        >
-          <input
-            className="border"
-            type="text"
-            value={taskText}
-            onChange={(e) => setTaskText(e.currentTarget.value)}
-          />
-          <button>タスクを作成</button>
-        </form>
+        <section className="px-1">
+          <div className="flex pl-8">
+            <h1 className="flex-1 text-center font-bold">
+              <input
+                disabled={props.disabled}
+                className="inline-block w-full rounded py-1 text-center focus-visible:bg-gray-200 dark:focus-visible:bg-gray-700"
+                placeholder={t("Task list name")}
+                value={taskList.name}
+                onChange={onTaskListNameChange}
+              />
+            </h1>
+            <AppPageLink
+              data-trigger={`sharing-${taskList.id}`}
+              tabIndex={props.disabled ? -1 : 0}
+              className="rounded p-1 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
+              href="/app"
+              params={{
+                sheet: "sharing",
+                tasklistid: taskList.id,
+                trigger: `sharing-${taskList.id}`,
+              }}
+              mergeParams
+            >
+              <Icon text="share" />
+            </AppPageLink>
+          </div>
+
+          <div className="flex items-center py-2">
+            <button
+              disabled={props.disabled}
+              className="flex rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
+              onClick={onInsertPositionIconClick}
+            >
+              {isInsertTop ? (
+                <Icon text="vertical_align_top" />
+              ) : (
+                <Icon text="vertical_align_bottom" />
+              )}
+            </button>
+
+            <form
+              className="flex flex-1 items-center py-2"
+              onSubmit={onTaskFormSubmit}
+            >
+              <input
+                data-tasktext
+                disabled={props.disabled}
+                className="flex-1 rounded-full border px-4 py-2 focus-visible:bg-gray-200 dark:focus-visible:bg-gray-700"
+                value={taskText}
+                placeholder={
+                  isInsertTop ? t("Add task to top") : t("Add task to bottom")
+                }
+                onChange={onTaskTextChange}
+                onKeyDown={onTaskTextKeyDownAndKeyUp}
+                onKeyUp={onTaskTextKeyDownAndKeyUp}
+                onBlur={onTaskTextBlur}
+              />
+              <button
+                disabled={props.disabled}
+                className="flex rounded p-2 focus-visible:bg-gray-200 dark:fill-white dark:focus-visible:bg-gray-700"
+                type="submit"
+              >
+                <Icon text="send" />
+              </button>
+            </form>
+          </div>
+        </section>
+        <section className="flex fill-gray-400 p-1 pl-2 text-gray-400">
+          <button
+            disabled={props.disabled}
+            className="flex rounded p-1 focus-visible:bg-gray-200 dark:focus-visible:bg-gray-700"
+            onClick={onSortTasksButtonClick}
+          >
+            <Icon text="sort" />
+            <span className="pl-1">{t("Sort")}</span>
+          </button>
+
+          <div className="inline-block flex-1"></div>
+
+          <button
+            disabled={props.disabled}
+            className="flex rounded p-1 focus-visible:bg-gray-200 dark:focus-visible:bg-gray-700"
+            onClick={onClearCompletedTasksButtonClick}
+          >
+            <span className="pr-1">{t("Clear Completed")}</span>
+            <Icon text="delete" />
+          </button>
+        </section>
       </header>
 
       <div>

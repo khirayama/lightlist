c29f86a132dc2f04bfd4007cfaca0deb076439f0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-12-28 16:33:31 +0900

    Create register endpoint

---
+++ src/pages/api/register.ts
@@ -0,0 +1,85 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { App as AppType, TaskList as TaskListType } from "@prisma/client";
+import * as Y from "yjs";
+import { v4 as uuid } from "uuid";
+
+import { prisma, exclude, auth } from "libs/apiHelper";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { user, errorMessage } = await auth(req);
+  if (errorMessage) {
+    return res.status(401).json({ error: errorMessage });
+  }
+
+  if (req.method === "POST") {
+    const lang = req.body.lang || "JA";
+
+    const appDoc = new Y.Doc();
+    const ad = appDoc.getMap("app");
+    ad.set("userId", user.id);
+    ad.set("taskListIds", []);
+    ad.set("taskInsertPosition", "TOP");
+    ad.set("online", false);
+
+    const [app, profile, preferences] = await prisma.$transaction([
+      prisma.app.create({
+        data: {
+          ...ad.toJSON(),
+          update: Y.encodeStateAsUpdate(appDoc),
+        } as AppType,
+      }),
+      prisma.profile.create({
+        data: {
+          userId: user.id,
+          displayName: "",
+        },
+      }),
+      prisma.preferences.create({
+        data: {
+          userId: user.id,
+          lang,
+          theme: "SYSTEM",
+        },
+      }),
+    ]);
+
+    if (!app || !profile || !preferences) {
+      return res.status(500).json({ error: "Failed to create user" });
+    }
+
+    const id = uuid();
+    const taskListDoc = new Y.Doc();
+    const td = taskListDoc.getMap(id);
+    td.set("id", id);
+    td.set("name", `📋 ${lang === "JA" ? "個人" : "PERSONAL"}`);
+    const tasks = new Y.Array();
+    td.set("tasks", tasks);
+
+    const [taskList, shareCode] = await prisma.$transaction([
+      prisma.taskList.create({
+        data: {
+          ...td.toJSON(),
+          update: Y.encodeStateAsUpdate(taskListDoc),
+        } as TaskListType,
+      }),
+      prisma.shareCode.create({
+        data: {
+          taskListId: id,
+        },
+      }),
+    ]);
+
+    return res.json({
+      app: exclude(app, ["id", "userId"]),
+      profile: exclude(profile, ["id", "userId"]),
+      preferences: exclude(preferences, ["id", "userId"]),
+      taskList: {
+        ...taskList,
+        shareCode: shareCode.code,
+      },
+    });
+  }
+}

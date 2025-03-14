7bd685b62010c7a0db4651b909e4944c10c909ce
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-03-14 22:28:36 +0900

    Create AppMain component

---
+++ src/components/AppMain.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import qs from "query-string";
+
+import {
+  Carousel,
+  CarouselList,
+  CarouselItem,
+  CarouselIndicator,
+} from "components/primitives/Carousel";
+import { TaskList } from "components/TaskList";
+
+function getTaskListIdIndex(taskListIds: string[]) {
+  const taskListId = (qs.parse(window.location.search).taskListId ||
+    "") as string;
+  const taskListIndex = taskListIds.includes(taskListId)
+    ? taskListIds.indexOf(taskListId)
+    : 0;
+  return taskListIndex;
+}
+
+export function AppMain({ app, taskLists }) {
+  const [index, setIndex] = useState(getTaskListIdIndex(app.taskListIds));
+
+  useEffect(() => {
+    setIndex(getTaskListIdIndex(app.taskListIds));
+  }, [window.location.search, app]);
+
+  return (
+    <Carousel
+      index={index}
+      onIndexChange={(idx) => {
+        const taskListId = app.taskListIds[idx];
+        console.log("TODO: move to task list and update URL");
+      }}
+    >
+      <CarouselIndicator />
+      <CarouselList>
+        {app.taskListIds.map((taskListId) => {
+          const taskList = taskLists[taskListId];
+          return (
+            <CarouselItem key={taskList.id}>
+              <TaskList taskList={taskList} app={app} />
+            </CarouselItem>
+          );
+        })}
+      </CarouselList>
+    </Carousel>
+  );
+}

0b99e05cd44264fe067ab3bd8e2eebb15adfc3d1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-03-15 16:27:08 +0900

    Move createInitialGlobalState to config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,32 @@
 export const config = {
-  appBaseUrl: "/new",
+  appBaseUrl: "/app",
 };
+
+export function createInitialState(): GlobalState {
+  return {
+    auth: {
+      session: null,
+    },
+    app: {
+      taskInsertPosition: "BOTTOM",
+      taskListIds: [],
+      update: new Uint8Array(),
+    },
+    profile: {
+      displayName: "",
+      email: "",
+    },
+    preferences: {
+      lang: "EN",
+      theme: "SYSTEM",
+    },
+    taskLists: {},
+    isInitialized: {
+      auth: false,
+      app: false,
+      profile: false,
+      preferences: false,
+      taskLists: false,
+    },
+  };
+}

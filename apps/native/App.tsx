b896674ed60a93fc41a7c6dd8e6e883fd46261e5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-09-09 21:18:56 +0900

    Update i18n for native

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import './src/i18n';
+import './src/styles/global.css';
 
 export default function App() {
   const { t } = useTranslation();
 
   return (
-    <View className="flex-1 bg-white items-center justify-center">
+    <View className="flex-1 bg-red-400 items-center justify-center">
       <Text className="text-2xl font-bold mb-4">{t('welcome')}</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />

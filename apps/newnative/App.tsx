b25cf46e4b11eb9058420b4a3043382613355c21
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-09-10 21:32:27 +0900

    Create new native app

---
+++ apps/newnative/App.tsx
@@ -0,0 +1,33 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import "./src/global.css"
+//
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+import "./src/global.css"
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-red-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}

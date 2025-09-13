273293523097a6fa026d59ed4d6a8c470a905516
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-09-13 20:18:28 +0900

    Create components/Main

--- apps/newnative/App.tsx
+++ apps/newnative/App.tsx
@@ -1,28 +1,7 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import "./src/global.css"
-//
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.tsx to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 import "./src/global.css"
-import { Text, View } from "react-native";
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
+import Main from "./src/components/Main";
 
 i18n
   .use(initReactI18next)
@@ -47,13 +26,5 @@ i18n
   });
  
 export default function App() {
-  const { t } = useTranslation();
-
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        {t('Welcome to Nativewind')}
-      </Text>
-    </View>
-  );
+  return <Main />;
 }

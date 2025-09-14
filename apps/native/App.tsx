8a1750e650e58c280168bd6288e0e78a356ea5df
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-09-14 15:05:31 +0900

    Replace native with newnative

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,17 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { useTranslation } from 'react-i18next';
-import './src/i18n';
-import './src/styles/global.css';
+import "./src/global.css"
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Main from "./src/components/Main";
 
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          "Welcome to Nativewind": "Welcome to Nativewind",
+        }
+      },
+      ja: {
+        translation: {
+          "Welcome to Nativewind": "Nativewindへようこそ",
+        }
+      }
+    },
+    lng: "ja",
+    fallbackLng: "ja",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+ 
 export default function App() {
-  const { t } = useTranslation();
-
-  return (
-    <View className="flex-1 bg-red-400 items-center justify-center">
-      <Text className="text-2xl font-bold mb-4">{t('welcome')}</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />;
 }

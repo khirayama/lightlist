4070039f8fcd9a1394e79e54de46c4b9a3df4a09
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-10-15 21:15:15 +0900

    Update apps/native configs

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = true;
+config.resolver.extraNodeModules = {
+  lib: path.resolve(__dirname, '../../packages/lib/src'),
+};
 
 module.exports = withNativeWind(config, { input: './src/global.css' });

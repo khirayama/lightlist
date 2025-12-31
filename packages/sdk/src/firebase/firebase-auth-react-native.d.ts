c997b907811eb18f11e0b2471192391ae2fc7e97
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2025-12-31 13:40:11 +0900

    Remove needless file and lines

--- packages/sdk/src/firebase/firebase-auth-react-native.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Persistence, ReactNativeAsyncStorage } from "@firebase/auth";
-
-declare module "@firebase/auth" {
-  export function getReactNativePersistence(
-    storage: ReactNativeAsyncStorage,
-  ): Persistence;
-}

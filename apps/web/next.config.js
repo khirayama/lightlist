fb8d7a8d20c84d37a63613ced2ed59f758a3bd63
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2026-02-24 08:39:12 +0900

    Update next config for apps/web

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,6 +9,23 @@ const nextConfig = {
       "@dnd-kit/sortable",
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Content-Type",
+            value: "application/javascript; charset=utf-8",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

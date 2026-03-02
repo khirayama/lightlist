a5e9c240db6d468ecd8d7373c3169c31b2d602a4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2026-03-03 05:10:07 +0900

    Update config files

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/.turbo/**",
+      "**/.expo/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/ios/**",
+      "**/android/**",
+      "**/coverage/**",
+    ],
+  },
+  {
+    files: ["**/*.{js,cjs,mjs,ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {},
+  },
+];
